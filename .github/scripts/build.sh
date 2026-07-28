1248cc1ca6d2a32f79c0b5998a8fb52c951b1ad4
ztyxlv <ztyxlv@users.noreply.github.com>
2026-07-28 20:49:01 +0800

    Updated on 2026-07-28 a下 20:49:01

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -149,6 +149,7 @@ sync_rules() {
   git config user.email "github-actions[bot]@users.noreply.github.com"
 
   git add "$path"
+  git add -f "$path/source"
 
   if ! git diff --cached --quiet; then
     git commit -m "Updated on $(TZ=Asia/Shanghai date '+%F at %T')"
